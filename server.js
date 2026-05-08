a006c751aaaa278df2e14419667ebba810f3c01a
Ishaq <auwalishaq113@gmail.com>
2026-05-08 12:59:12 -0700

    com

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const app = express();
 connectDB();
 seed();
 // Middleware
-app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(cors({ origin: '*',
+  methods:['GET', 'POST', 'PUT'],
+  allowHeaders: ['Content-Type', 'Authorization']
+ }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
